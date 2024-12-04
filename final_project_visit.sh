0e6ec334deb6ce551c851aa65212ad9eeab7fffb
mdsteen <msteen@uci.edu>
2024-12-04 10:36:19 -0800

    Sort function edit

--- final_project_visit.sh
+++ final_project_visit.sh
@@ -5,3 +5,5 @@ gawk -F, '{print $2 "," $7}' IRC_SortedSpecies_modified_site.csv | uniq -c > spe
 
 #Calculates the percent frequency of visits for each species/site pairing.
 awk -F, '{split($1, arr, " "); count[arr[2]] += arr[1]; site[arr[2]","$2] += arr[1]} END {for (species in count) {for (site_name in site) if (site_name ~ species) print site_name, (site[site_name]/count[species])*100 "%"}}' species_site_counts.txt
+
+awk -F, '{split($1, arr, " "); count[arr[2]] += arr[1]; site[arr[2]","$2] += arr[1]} END {for (species in count) {for (site_name in site) if (site_name ~ species) print site_name, (site[site_name]/count[species])*100 "%"}}' species_site_counts.txt | sort
